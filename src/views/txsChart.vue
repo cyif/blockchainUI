92df4c442baef23b404c8cd6e84b2892b7fe297e
yushu <827242638@qq.com>
2017-05-15 16:09:42 +0800

    aini

--- src/views/txsChart.vue
+++ src/views/txsChart.vue
@@ -21,7 +21,7 @@
 </template>
 <script>
     import txsStats from '../components/sheet/Stats_txs.vue'
-    import txsTime from '../components/graph/txs_time.vue'
+    import txsTime from '../components/graph/contract.vue'
     import txsNum from '../components/graph/txsNum.vue'
     import txsBlock from '../components/graph/txs_Block.vue'
     import confirmation from '../components/graph/confirmationTime.vue'
